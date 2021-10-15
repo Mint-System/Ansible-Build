e483783b53545617d2daef69fc33fa25133c26a0
Janik von Rotz <login@janikvonrotz.ch>
2021-10-15 16:05:01 +0200

    add favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
     title: 'Ansible Playbooks',
     description: 'Collection of Ansible playbooks and roles.',
+    head: [
+        ['link', { rel: "icon", type: "image/png", href: "icon.png"}],
+    ],
     themeConfig: {
         sidebar: 'auto',
         repo: 'mint-system/ansible-playbooks',
