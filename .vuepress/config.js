173138ec358e6a5cca9850cc448280b0cfa5713f
janikvonrotz <contact@janikvonrotz.ch>
2022-05-06 17:01:43 +0200

    add mint link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,12 +5,14 @@ module.exports = {
         ['link', { rel: "icon", type: "image/png", href: "icon.png"}],
     ],
     themeConfig: {
+        logo: '/icon.png',
         sidebar: 'auto',
         repo: 'mint-system/ansible-playbooks',
         docsBranch: 'master',
         editLinks: true,
         nav: [
-            { text: 'Home', link: '/' }
+            { text: 'Home', link: '/' },
+            { text: 'Mint System', link: 'https://www.mint-system.ch' }
         ],
     },
     dest: 'public',
