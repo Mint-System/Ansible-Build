5ab7bb6185af3bd753805c9f783749d7f227049a
janikvonrotz <contact@janikvonrotz.ch>
2023-01-04 18:16:04 +0100

    udpate template

--- roles/odoo-data/templates/__manifest__.py
+++ roles/odoo-data/templates/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Mint System GmbH, Odoo Community Association (OCA)',
     'website': 'https://www.mint-system.ch',
     'category': 'Technical',
-    'version': '14.0.1.0.0',
+    'version': '1.0.0',
     'license': 'AGPL-3',
     
     'depends': ['{{ depends }}'],
