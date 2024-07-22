aab5e90e8eecfd815065967254d82212a3fb0d68
Janik von Rotz <login@janikvonrotz.ch>
2024-07-22 13:56:02 +0200

    feat: update vueppress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,19 @@
-import { defaultTheme } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
 import { searchPlugin } from '@vuepress/plugin-search'
 import { plausiblePlugin } from './plausible'
 import { defineUserConfig } from 'vuepress'
 
 
 export default defineUserConfig({
+    bundler: viteBundler(),
     lang: 'en-US',
     title: 'Ansible Build',
     description: 'Collection of Ansible playbooks and roles.',
     head: [
         ['link', { rel: 'icon', href: '/icon.png' }]
     ],
+    pagePatterns: ['**/*.md', '!.vuepress', '!node_modules', '!tmp', '!venvmaster'],
     theme: defaultTheme({
         logo: '/icon.png',
         repo: 'mint-system/ansible-build',
